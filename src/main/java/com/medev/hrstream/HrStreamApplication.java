6074dc1298f4abe0b01d2fd8b6530a678fd8a9a4
mehdi mediani <mediani.mehdi.dev@gmail.com>
2026-03-10 14:22:01 +0000

    Add auto-initialization of admin user if not exists

--- src/main/java/com/medev/hrstream/HrStreamApplication.java
+++ src/main/java/com/medev/hrstream/HrStreamApplication.java
@@ -1,8 +1,14 @@
 package com.medev.hrstream;
 
+import com.medev.hrstream.user.Role;
+import com.medev.hrstream.user.User;
+import com.medev.hrstream.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableJpaAuditing
 @SpringBootApplication()
@@ -12,4 +18,19 @@ public class HrStreamApplication {
         SpringApplication.run(HrStreamApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner init(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.findByEmail("admin@hrstream.local").isEmpty()) {
+                User admin = User.builder()
+                        .firstname("Admin")
+                        .lastname("User")
+                        .email("admin@hrstream.local")
+                        .password(passwordEncoder.encode("Admin#1234"))
+                        .role(Role.ADMIN)
+                        .build();
+                userRepository.save(admin);
+            }
+        };
+    }
 }
